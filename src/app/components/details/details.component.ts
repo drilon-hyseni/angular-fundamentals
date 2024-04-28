ec3d1c18602d4678e696076dcd47294abe955a04
Drilon Hyseni <emilianhyseni@gmail.com>
2024-04-28 19:07:11 +0200

    Setting up Bootstrap

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { DetailsReviewsComponent } from '../details-reviews/details-reviews.component';
+import { DetailsMoviesComponent } from '../details-movies/details-movies.component';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [DetailsReviewsComponent, DetailsMoviesComponent],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
 })
-export class DetailsComponent {
+export class DetailsComponent
+  implements OnInit, OnChanges, AfterContentInit, AfterViewInit
+{
+  constructor() {}
 
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+  }
 }
