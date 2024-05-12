50e5447d2b2289b5f853d95d7c27426dcc691d3e
Drilon Hyseni <emilianhyseni@gmail.com>
2024-05-12 18:10:11 +0200

    Custom Attribute element

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,24 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderDirective } from '../../directives/header.directive';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  currentStyles: Record<string, string> = {};
+  constructor() {}
 
-  isCentered = true;
-  isSuccess = false;
-  isLarge = true;
-  constructor() {
-    this.currentStyles = {
-      'text-align': this.isCentered ? 'center' : '',
-      color: this.isSuccess ? 'green' : 'red',
-      'font-size': this.isLarge ? 'large' : 'small',
-    };
-  }
+  // currentStyles: Record<string, string> = {};
+  // isCentered = true;
+  // isSuccess = false;
+  // isLarge = true;
+  // constructor() {
+  //   this.currentStyles = {
+  //     'text-align': this.isCentered ? 'center' : '',
+  //     color: this.isSuccess ? 'green' : 'red',
+  //     'font-size': this.isLarge ? 'large' : 'small',
+  //   };
+  // }
 }
